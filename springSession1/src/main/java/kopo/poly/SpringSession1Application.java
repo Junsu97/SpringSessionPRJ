c7f0d7e7a791877c2a0442d9983fd2ec12eae730
junsu97 <wnstn7759@naver.com>
2024-08-28 10:01:03 +0900

    Spring Session + Redis 테스트 완

---
+++ springSession1/src/main/java/kopo/poly/SpringSession1Application.java
@@ -0,0 +1,15 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+@SpringBootApplication
+public class SpringSession1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSession1Application.class, args);
+    }
+
+}
---
+++ springSession2/src/main/java/kopo/poly/SpringSession2Application.java
@@ -0,0 +1,15 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+@SpringBootApplication
+public class SpringSession2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSession2Application.class, args);
+    }
+
+}
